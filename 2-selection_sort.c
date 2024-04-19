e267187839ec1ac12622e71a6ada0d9425ca78a4
Tanner Saint <tsaint2004@gmail.com>
2024-04-19 04:09:18 +0000

    added code to 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ *
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i;
+	unsigned int j;
+	int minimum;
+	int swap;
+	int save_index;
+	int temp;
+
+	if (array == NULL)
+	{
+		return;
+	}
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minimum = array[i];
+		save_index = i;
+		swap = 0;
+		for (j = i + 1; j < size; j++)
+		{
+			if (minimum > array[j])
+			{
+				minimum = array[i];
+				swap = 1;
+				save_index = j;	
+			}
+		}
+		if (swap == 1)
+		{
+			temp = array[i];
+			array[i] = array[save_index];
+			array[save_index] = temp;
+			print_array(array, size);
+		}
+	}
+}

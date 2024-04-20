fbe515f892b2f19069e57af41b1007d1bd2fabce
Tanner Saint <tsaint2004@gmail.com>
2024-04-20 01:13:02 +0000

    added code to 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+/**
+ *
+*/
+
+void node_swap(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+
+	if (node2->next != NULL)
+	{
+		node2->next->prev = *node1;
+	}
+
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+
+	if ((*node1)->prev != NULL)
+	{
+		(*node1)->prev->next = node2;
+	}
+	else
+	{
+		*head = node2;
+	}
+
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ *
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	listint_t *temp;
+	listint_t *back_node;
+
+    if (list == NULL || *list == NULL)
+    {
+        return;
+    }
+
+    for (current = (*list)->next; current != NULL; current = temp)
+	{
+		temp = current->next;
+		back_node = current->prev;
+
+		while(back_node != NULL && current->n < back_node->n)
+		{
+			node_swap(list, &back_node, current);
+			print_list(*list);
+		}
+	}
+}
